5095eba55d03cdf032fd274da4b180d04c05bd64
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-11-07 15:35:00 +0300

    completed assignment

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,5 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  #has_many :bakeries
+  belongs_to :bakery
 end
--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -1,3 +1,5 @@
 class Bakery < ActiveRecord::Base
   # add association macro here
+  #belongs_to :baked_good
+  has_many :baked_goods
 end
